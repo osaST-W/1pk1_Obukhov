7cfce08b786445fd497b56cd2775026a3a99ed52
Станислав Обухов <guestice04@gmail.com>
2025-01-28 19:36:36 +0500

    Решены здания 2 урока

---
+++ Task_02_02/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task_02_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Найти значение выражения при a = 8, b = 14, с = π/4
+            double a = 8;
+            double b = 14;
+            double c = Math.PI / 4;
+            double part1 = Math.Sqrt(Math.Pow(a - 1, 3));
+            double part2 = Math.Sqrt(Math.Pow(b + part1, 4));
+            double part3 = Math.Abs(a - b);
+            double part4 = Math.Pow(Math.Abs(c), 2);
+            double part5 = Math.Tan(c);
+            double part6 = part4 + part5;
+            double part7 = (part2 / part4 * part6);
+            Console.WriteLine(part7);
+
+        }
+    }
+}
---
+++ Task_02_04/Program.cs
@@ -0,0 +1,111 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.ConstrainedExecution;
+
+namespace Task_02_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+       
+            
+            {
+            { 
+            /*Пользователь вводит свою дату рождения построчно (сначала год, потом месяц и в конце дату) произведите
+            расчет является ли пользователь совершеннолетним на текущую дату и выведите соответствующее сообщение об
+            этом*/
+
+            Console.WriteLine("Введите вашу дату рождения( год, месяц, день)");
+            int g = Int32.Parse(Console.ReadLine()); //год
+            
+            int m = Int32.Parse(Console.ReadLine()); //месяц
+            
+            int d = Int32.Parse(Console.ReadLine()); //день
+                Console.Clear();
+            
+            do //внешний цикл
+            {
+                if (g <= 2025) 
+                {
+                    Console.WriteLine("Год: "+ g);
+                }
+                else if ( g> 2025)
+                {
+                    Console.WriteLine("Введен некореектный возраст");
+                }
+                break;
+            }
+            while (true);
+
+            do //внеш цикл
+            {
+                if (m <= 12) 
+                {
+                    Console.WriteLine("Месяц: " + m);
+                }
+                else if (m > 12) 
+                {
+                    Console.WriteLine("Введен некореектный возраст");
+                }
+                break;
+            }
+            while (true);
+
+            do //внешний цикл
+            {
+                if (d <= 31) // внутр цикл 
+                {
+                    Console.WriteLine("День: " + d);
+                }
+                else if (d > 31) 
+                {
+                    Console.WriteLine("Введен некореектный возраст");
+                }
+                break;
+            }
+            while (true);
+
+                // Получаем текущую дату
+                int currentG = 2025; // Введите текущий год
+                int currentM = 1; // Введите текущий месяц
+                int currentD = 28; // Введите текущий день
+
+                // Проверка на совершеннолетие
+                bool isAdult = false;
+
+                if (currentG > g + 18) // внеш цикл
+                {
+                    isAdult = true;
+                }
+                else if (currentG == g + 18)
+                {
+                    if (currentM > m) // внутр цикл 1
+                    {
+                        isAdult = true;
+                    }
+                    else if (currentM == m)
+                    {
+                        if (currentD >= d) // внутр цикл 2
+                        {
+                            isAdult = true;
+                        }
+                    }
+                }
+
+                // Вывод результата
+                if (isAdult)
+                {
+                    Console.WriteLine("Вы совершеннолетний.");
+                }
+                else
+                {
+                    Console.WriteLine("Вы несовершеннолетний.");
+                }
+
+
+
+
+            }
+        }
+    }
+}
+
